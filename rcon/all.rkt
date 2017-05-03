309377e34aed3154791cd8f73dc14eff4408620a
Matthew Butterick <mb@mbtype.com>
2017-05-03 15:37:02 -0700

    adjustments

--- rcon/all.rkt
+++ rcon/all.rkt
@@ -2,6 +2,7 @@
 (require plt-web/style
          racket/runtime-path
          racket/path
+         racket/format
          raco/all-tools
          "resources.rkt"
          "utils.rkt"
@@ -47,17 +48,24 @@
                       (excluded-path? fn)
                       (and current? (equal? fn (string->path "index.html")))))
         (copyfile #:site con-site (build-path starting-dir fn)
-                  (format "~a/~a" year fn)))
+                  (string-join (map ~a (append
+                                        (if current? null (list year))
+                                        (list fn))) "/")))
   (for* ([p (in-directory (build-path starting-dir "fonts"))])
-        (copyfile #:site con-site p (format "~a/fonts/~a" year (filename p)))))
+        (copyfile #:site con-site p
+                  (string-join (map ~a (append
+                                        (if current? null (list year))
+                                        (list "fonts" (filename p)))) "/"))))
 
 (define-runtime-path 2015-dir "2015")
 (pollen-rebuild! 2015-dir)
 (copy-con-site! 2015-dir 2015)
 
+
 (define-runtime-path 2016-dir "2016")
 (pollen-rebuild! 2016-dir)
 (copy-con-site! 2016-dir 2016)
+
 
 (define-runtime-path 2017-dir "2017")
 (pollen-rebuild! 2017-dir)
