dd04872c1211e2b2e9121eb86c367b362195e6bb
Matthew Butterick <mb@mbtype.com>
2019-06-29 18:14:43 -0700

    simplify

--- rcon/2019/pollen.rkt
+++ rcon/2019/pollen.rkt
@@ -18,27 +18,14 @@
 (define (name->label fn ln)
   `(label (barcode ,fn) (fn ,fn) (ln ,ln) (week "Racket Week 2019")))
 
-(define-tag-function (lanyard-names attrs names)
-  (define label-names
-    (for/list ([name (in-list names)]
-               #:unless (equal? name "\n"))
-      (match (string-split name " ")
-        [(cons fn lns) (list fn (string-join lns " "))])))
+(define-tag-function (root attrs names)
+  (define label-names (for/list ([name (in-list names)]
+                                 #:unless (equal? name "\n"))
+                        (match (string-split name " ")
+                          [(cons fn lns) (list fn (string-join lns " "))])))
   (cons
    'lanyard-names
-   (for/list ([2-label-names (in-slice 2 label-names)])
-     (match-define (list (list fn1 ln1) (list fn2 ln2)) 2-label-names)
-     (define label-1 (name->label fn1 ln1))
-     (define label-2 (name->label fn2 ln2))
-     (list 'labels
-           label-1
-           label-2
-           (attr-set label-1 'class "invert")
-           (attr-set label-2 'class "invert")))))
-
-(define root lanyard-names)
-
-(define-tag-function (label attrs items)
-  (decode `(label ,attrs ,@items)
-          #:txexpr-elements-proc (λ (es) (decode-paragraphs es #:force? #t))))
+   (for/list ([two-names (in-slice 2 label-names)])
+     (match-define labels (map (λ (n) (apply name->label n)) two-names))
+     (cons 'labels (append labels (map (λ (label) (attr-set label 'class "invert")) labels))))))
 
