317098f84eb3de847e4e76c31532ddfea96a98e0
Matthew Butterick <mb@mbtype.com>
2019-06-29 17:47:52 -0700

    simplify

--- rcon/2019/lanyard.html.pm
+++ rcon/2019/lanyard.html.pm
@@ -1,26 +1,5 @@
-#lang debug br
-(require racket/sequence racket/match racket/string pollen/tag)
+#lang pollen
 
-(define label (default-tag-function 'label))
-(define root (default-tag-function 'root))
-
-(define (lanyard-names . names)
-   (define label-names
-     (for/list ([name names]
-                #:unless (equal? name "\n"))
-       (match-define (cons fn lns) (string-split name " "))
-       (list fn (string-join lns " "))))
-   `(lanyard-names
-         ,@(for*/list ([2-label-names (in-slice 2 label-names)])
-           (match-define (list (list fn1 ln1) (list fn2 ln2)) 2-label-names)
-             `(labels
-             (label (barcode ,fn1) (fn ,fn1) (ln ,ln1) (week "Racket Week 2019"))
-             (label (barcode ,fn2) (fn ,fn2) (ln ,ln2) (week "Racket Week 2019"))
-             (label ((class "invert")) (barcode ,fn1) (fn ,fn1) (ln ,ln1) (week "Racket Week 2019"))
-             (label ((class "invert")) (barcode ,fn2) (fn ,fn2) (ln ,ln2) (week "Racket Week 2019"))))))
-(define doc
-  ◊root{
-◊lanyard-names{
 Anders Pitman
 James Moody
 Guy Watson
@@ -33,6 +12,3 @@ Kieron Hardy
 Dionna Glaze
 Sean Kanaley
 Wei Li
-}})
-
-(provide doc)
